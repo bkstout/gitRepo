98c089d96823722bbd7a19d73b47fa520a32edb6
bkstout <bkstout@gmail.com>
2016-06-04 15:20:57 -0400

    excercise complete

--- playground/FunctionExcercise.playground/Contents.swift
+++ playground/FunctionExcercise.playground/Contents.swift
@@ -2,4 +2,30 @@
 
 import UIKit
 
-var str = "Hello, playground"
+func add(num1: Double, num2: Double) -> Double {
+    return num1 + num2
+}
+
+add(5, num2: 5)
+
+func subtract(num3: Int, num4: Int) -> Int {
+    if num3 > num4 {
+        return num3 - num4
+    } else {
+        return num4 - num3
+    }
+}
+
+subtract(5, num4: 17)
+
+func multiply(num5: Float, num6: Float) -> Float {
+    return num5 * num6
+}
+
+multiply(5, num6: 2)
+
+func divide(num7: Double, num8: Double) -> Double {
+    return num7 / num8
+}
+
+divide(17, num8: 4)
