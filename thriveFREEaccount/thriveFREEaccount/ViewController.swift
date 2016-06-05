7075a53040d27429cbf16670d3049164a940f477
bkstout <bkstout@gmail.com>
2016-06-04 22:15:01 -0400

    thrive updates

--- thriveFREEaccount/thriveFREEaccount/ViewController.swift
+++ thriveFREEaccount/thriveFREEaccount/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var thriveExperience: UIImageView!
     @IBOutlet weak var CapsulesMen: UIButton!
+    @IBOutlet weak var freeAccount: UIButton!
     @IBOutlet weak var myWebView: UIWebView!
     
     override func viewDidLoad() {
@@ -25,11 +26,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getMensCapsules(sender: AnyObject) {
-        // print("Getting information on capsules for men")
         let capMenUrl = NSURL (string: "https://media.le-vel.com/Documents/THRV001M.pdf");
         let requestObj = NSURLRequest(URL: capMenUrl!);
         myWebView.loadRequest(requestObj);
     }
 
+    @IBAction func makeFreeAccount(sender: AnyObject) {
+        let freeAcctUrl = NSURL (string: "https://bkstout.le-vel.com/login");
+        let requestAct = NSURLRequest(URL: freeAcctUrl!);
+        myWebView.loadRequest(requestAct);
+
+    }
 }
 
