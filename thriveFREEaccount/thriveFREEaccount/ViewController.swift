62af3590eab90c66bcf4b134c94ca64046091824
bkstout <bkstout@gmail.com>
2016-06-04 21:47:26 -0400

    thrive updates

--- thriveFREEaccount/thriveFREEaccount/ViewController.swift
+++ thriveFREEaccount/thriveFREEaccount/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thriveExperience: UIImageView!
+    @IBOutlet weak var CapsulesMen: UIButton!
+    @IBOutlet weak var myWebView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getMensCapsules(sender: AnyObject) {
+        // print("Getting information on capsules for men")
+        let capMenUrl = NSURL (string: "https://media.le-vel.com/Documents/THRV001M.pdf");
+        let requestObj = NSURLRequest(URL: capMenUrl!);
+        myWebView.loadRequest(requestObj);
+    }
 
 }
 
