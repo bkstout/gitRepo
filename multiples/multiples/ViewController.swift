00698df9bc69777243ae7f4dcb6fc1eac92042b4
bkstout <bkstout@gmail.com>
2016-06-12 22:00:53 -0400

    end of day update

--- multiples/multiples/ViewController.swift
+++ multiples/multiples/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     //Variables
     var returnMessage = ""
+    var newCalcNum = 0
+    var sumNum = 0
+    var myMultiplier = 0
+    let maxNum = 100
     
     //Outlets
     @IBOutlet weak var multiplesImg: UIImageView!
@@ -32,13 +36,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     */
+    
+    func enoughIsEnough() -> Bool {
+        if sumNum >= maxNum {
+            return true
+        }
+        return false
+    }
+    
+    func resetGame() {
+        multiplesImg.hidden = false
+        playBtn.hidden = false
+        multipleField.hidden = false
+        addBtn.hidden = true
+        pressToAddLbl.hidden = true
+        sumNum = 0
+        myMultiplier = 0
+        newCalcNum = 0
+        multipleField.text = nil
+        pressToAddLbl.text = "Press To Add Label"
+    }
 
     @IBAction func pressPlayBtn(sender: AnyObject) {
-        multiplesImg.hidden = true
-        playBtn.hidden = true
-        multipleField.hidden = true
-        addBtn.hidden = false
-        pressToAddLbl.hidden = false
+        if multipleField.text != nil && multipleField.text != "" {
+            multiplesImg.hidden = true
+            playBtn.hidden = true
+            multipleField.hidden = true
+            addBtn.hidden = false
+            pressToAddLbl.hidden = false
+            myMultiplier = Int(multipleField.text!)!
+        } else {
+            resetGame()
+            // multipleField.text = "ERROR:  Please Enter A Number"
+        }
+        
+    }
+    @IBAction func pressAddBtn(sender: AnyObject) {
+        sumNum += Int(multipleField.text!)!
+        pressToAddLbl.text = "\(newCalcNum) + \(myMultiplier) = \(sumNum)"
+        newCalcNum = sumNum
+        if enoughIsEnough() {
+            resetGame()
+        }
     }
 }
 
